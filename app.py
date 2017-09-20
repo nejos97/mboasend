9e9ed53b0e18153d0e1a1a2c579f0af06994d169
Nenba Jonathan <nenbajonathan@protonmail.com>
2017-09-20 17:56:12 +0100

    set all file to repo

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import url_for
-from flask import flash
-from flask import make_response
-from UrlClass import URL
-from werkzeug import generate_password_hash
-import pickle
+import os,sys
+from flask import Flask,render_template,request,redirect,url_for,flash,make_response,session
 
 app = Flask(__name__)
 app.secret_key = "Hello"
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600*24*30
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/")
+def index():
+    return render_template("index.html",title="Home")
+
+
+@app.route("/todo/", methods=['GET','POST'])
 def todo():
-    if not request.cookies.get("auth"):
+    if not "auth" in session :
         return redirect(url_for("login"))
     t=[]
     with open("donnee.dat","r") as f :
@@ -27,27 +25,35 @@ def todo():
     t.reverse()
     return render_template("todo.html", title="Todo list", todo = t)
 
-@app.route("/login",methods=['GET','POST'])
+@app.route("/upload")
+def upload():
+    liste = os.listdir("uploads")
+    return render_template("uploads.html",title="Uploads", fichiers = liste)
+
+@app.route("/about")
+def about():
+    pass
+
+@app.route("/login/",methods=['GET','POST'])
 def login():
     error = None
-    if request.cookies.get("auth"):
+    if "auth" in session :
         flash("You are already logged in")
-        return redirect(url_for("todo"))
+        return redirect(url_for("index"))
     if request.method == 'POST':
         if request.form['username']=="m" and request.form['password']=="m" :
-            resp = make_response(redirect(url_for("todo")))
-            resp.set_cookie("auth","0")
-            return resp
+            session["auth"] = True
+            return redirect(url_for("index"))
         else :
             error = "username or password is invalide"
     return render_template("login.html",title="Login", error=error)
 
 @app.route("/logout/")
 def logout():
-    flash("You were successfully logged out")
-    resp = make_response(redirect(url_for("login")))
-    resp.set_cookie("auth","",expires=0)
-    return resp
+    if "auth" in session :
+        flash("You were successfully logout")
+        session.pop("auth",None)
+    return redirect(url_for("login"))
 
 @app.errorhandler(404)
 def error404(error):
